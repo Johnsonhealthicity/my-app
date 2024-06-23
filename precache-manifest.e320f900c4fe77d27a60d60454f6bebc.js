e443d4ecfe548a27127bb1a8f246489e2b4c39b6
Johnson <johnson.koil@healthicity.com>
2024-06-23 23:06:05 +0530

    Updates

---
+++ precache-manifest.e320f900c4fe77d27a60d60454f6bebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f536365b8992ddc00af2c88e76dc880",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c302b892783d68e323d3",
+    "url": "/my-app/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "d688ea63de3869687179",
+    "url": "/my-app/static/css/main.8b636d3b.chunk.css"
+  },
+  {
+    "revision": "c302b892783d68e323d3",
+    "url": "/my-app/static/js/2.5588a682.chunk.js"
+  },
+  {
+    "revision": "c22e7b4cb20d4556252c2aa78698d764",
+    "url": "/my-app/static/js/2.5588a682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d688ea63de3869687179",
+    "url": "/my-app/static/js/main.bff664d8.chunk.js"
+  },
+  {
+    "revision": "9fb6fedb38d1680e6ab4",
+    "url": "/my-app/static/js/runtime-main.38642c62.js"
+  }
+]);
